d037a4fe13db46028f7511a49e65e8fc5e3d973a
popatkaran <krn.ppt@gmail.com>
2026-04-27 13:02:19 +0200

    [popatkaran] - github pages settings

--- next.config.js
+++ next.config.js
@@ -1,14 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   transpilePackages: ['@terminal-bird/theme'],
   compress: true,
   poweredByHeader: false,
   reactStrictMode: true,
-  // Uncomment the following lines if deploying to GitHub Pages (static export):
-  // output: 'export',
   images: {
     formats: ['image/avif', 'image/webp'],
-    // unoptimized: true, // Required for static export
+    unoptimized: true,
   },
 };
 
